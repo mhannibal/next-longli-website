b580ee6819e32df77ef38a62913ebb595ed25a08
bessoufi mounir lenovo <bessoufi.mounir@gmail.com>
2023-09-08 10:37:20 +0100

    rewrite the script tag

--- app/google-analytics.tsx
+++ app/google-analytics.tsx
@@ -2,16 +2,10 @@
 
 import Script from "next/script"
 import * as gtag from "../gtag"
-import { usePathname, useSearchParams } from "next/navigation"
 import { useEffect } from "react";
+import { usePathname, useSearchParams } from "next/navigation"
 
 const GoogleAnalytics = () => {
-
-    //You can show in the console the GA_TRACKING_ID to confirm
-    //console.log(gtag.GA_TRACKING_ID)
-
-
-    
     const pathname = usePathname()
     const searchParams = useSearchParams()
   
@@ -21,35 +15,29 @@ const GoogleAnalytics = () => {
       }
     }, [pathname, searchParams])
   
-    if (process.env.NEXT_PUBLIC_VERCEL_ENV !== "production") {
-      return null
-    }
-  
+
     return (
-      <>
-        <noscript>
-          <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=${gtag.GA_TRACKING_ID}`}
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
-        <Script
-          id="gtm-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer', '${gtag.GA_TRACKING_ID}');
-    `,
-          }}
-        />
-      </>
-    )  
+        <>
+            <Script
+                strategy="afterInteractive"
+                src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+            />
+            <Script
+                id="gtag-init"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+                      window.dataLayer = window.dataLayer || [];
+                      function gtag(){dataLayer.push(arguments);}
+                      gtag('js', new Date());
+                      gtag('config', '${gtag.GA_TRACKING_ID}', {
+                      page_path: window.location.pathname,
+                      });
+                    `,
+                }}
+            />
+        </>
+    )
 }
 
 export default GoogleAnalytics
