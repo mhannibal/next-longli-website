ad2ef333ce8a40c8ad598a1b95c01b9dc27d0196
bessoufi mounir lenovo <bessoufi.mounir@gmail.com>
2023-10-04 21:48:52 +0100

    add some alert text and btn styling

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,10 @@
 export default function Login() {
     return (
-        <section className="p-16">
-        <a href="/auth/login">Login with google</a>
+        <section className="p-16 flex flex-col gap-4 justify-center items-center">
+            <h1 className=" bg-red-300 text-red-700 py-2 px-4 border border-red-600 rounded">هذه الصفحة محمية، يجب عليك تسجيل الدخول للمتابعة</h1>
+            <h1 className=" bg-red-300 text-red-700 py-2 px-4 border border-red-600 rounded">This page is protected, you should sign in to continue</h1>
+            <h1 className=" bg-red-300 text-red-700 py-2 px-4 border border-red-600 rounded">Cette page est protégée, vous devez vous connecter pour continuer</h1>
+            <a href="/auth/login" className="border border-gray-600 py-2 px-4 bg-pink-700 hover:bg-pink-500 text-white rounded bg-">Sign in  with google</a>
         </section>
         
     )
