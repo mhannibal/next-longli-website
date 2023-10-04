78f7d296b23824b3a26ecc3908bc7a506ecfaf17
bessoufi mounir lenovo <bessoufi.mounir@gmail.com>
2023-10-04 20:41:42 +0100

    changed the facebook link href

--- app/site-footer/index.tsx
+++ app/site-footer/index.tsx
@@ -8,7 +8,7 @@ export default function Footer(){
         <footer className='bg-mfooter-100 min-h-80 gap-8 text-white flex flex-col items-center justify-between  p-8 inclined-top-border'>
                 <div className="flex flex-col md:flex-row gap-4 ">
                     <button className="border rounded-xl p-4">
-                        <a href="https://www.facebook.com/profile.php?id=100094400536750" target="_blank">فيسبوك</a>
+                        <a href="https://www.facebook.com/longliapp" target="_blank">فيسبوك</a>
                     </button>
                     <button className="border rounded-xl p-4">
                         <Link href="https://instagram.com/longliapps/">انستجرام</Link>
