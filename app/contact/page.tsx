47cc9cbcb2c218248de2b9eead9480f7120b3e7a
bessoufi mounir lenovo <bessoufi.mounir@gmail.com>
2023-10-24 21:37:10 +0100

    add contact form functionality

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,10 @@
+import ContactForm from "./contact-form";
+
 export default function ContactPage(){
     return (
-        <h1>Contact</h1>
+        <section className='flex flex-col bg-white text-gray-900 items-center md:h-screen py-24'>
+            <h1>Contact</h1>
+            <ContactForm />
+        </section>
     )
 }
